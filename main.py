3aa7a1f8734daad8cd46866ed14079b888a3aa90
Ilyos-everest <143332901+Ilyos-everest@users.noreply.github.com>
2026-05-20 20:29:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,31 @@
 import os
-from flask import Flask, request
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
 import asyncio
+from flask import Flask
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
 
-TOKEN = os.getenv("8950640581:AAGZbbv2Ki98cmm-xJxRGdxdQTloYelNUss")
+TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 app = Flask(name)
 
-WEBHOOK_PATH = f"/{TOKEN}"
-WEBHOOK_URL = f"{os.getenv('RENDER_EXTERNAL_URL')}{WEBHOOK_PATH}"
-
-
 @dp.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer("✅ Professional Quiz Bot ishlayapti!")
-
+async def start_handler(message: Message):
+    await message.answer("✅ Bot ishlayapti!")
 
 @app.route("/")
 def home():
-    return "Bot is running"
+    return "Bot is live!"
 
-
-@app.route(WEBHOOK_PATH, methods=["POST"])
-def webhook():
-    update = types.Update.model_validate(request.json)
-
-    asyncio.run(dp.feed_update(bot, update))
-
-    return "ok"
-
-
-async def set_webhook():
-    await bot.set_webhook(WEBHOOK_URL)
-    print("Webhook:", WEBHOOK_URL)
-
+async def start_bot():
+    await dp.start_polling(bot)
 
 if name == "main":
-    asyncio.run(set_webhook())
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())
 
     app.run(
         host="0.0.0.0",
