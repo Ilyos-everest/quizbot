66fc765e5273b6407df941d3d90e88547f1f6f3f
Ilyos-everest <143332901+Ilyos-everest@users.noreply.github.com>
2026-05-20 20:21:03 +0500

    Refactor bot to use Flask and update webhook handling

--- main.py
+++ main.py
@@ -1,55 +1,48 @@
 import os
+from flask import Flask, request
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiohttp import web
+import asyncio
 
 TOKEN = os.getenv("8950640581:AAGZbbv2Ki98cmm-xJxRGdxdQTloYelNUss")
-WEBHOOK_HOST = os.getenv("WEBHOOK_HOST")
-
-WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+app = Flask(name)
+
+WEBHOOK_PATH = f"/{TOKEN}"
+WEBHOOK_URL = f"{os.getenv('RENDER_EXTERNAL_URL')}{WEBHOOK_PATH}"
+
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer("✅ Quiz Bot ishlayapti!")
+    await message.answer("✅ Professional Quiz Bot ishlayapti!")
 
 
-async def webhook_handler(request):
-    data = await request.json()
-
-    update = types.Update(**data)
-
-    await dp.feed_update(bot, update)
-
-    return web.Response()
+@app.route("/")
+def home():
+    return "Bot is running"
 
 
-async def on_startup(app):
+@app.route(WEBHOOK_PATH, methods=["POST"])
+def webhook():
+    update = types.Update.model_validate(request.json)
+
+    asyncio.run(dp.feed_update(bot, update))
+
+    return "ok"
+
+
+async def set_webhook():
     await bot.set_webhook(WEBHOOK_URL)
-    print("Webhook set:", WEBHOOK_URL)
-
-
-async def on_shutdown(app):
-    await bot.delete_webhook()
-
-
-app = web.Application()
-
-app.router.add_post(WEBHOOK_PATH, webhook_handler)
-
-app.on_startup.append(on_startup)
-app.on_shutdown.append(on_shutdown)
+    print("Webhook:", WEBHOOK_URL)
 
 
 if name == "main":
-    port = int(os.environ.get("PORT", 10000))
+    asyncio.run(set_webhook())
 
-    web.run_app(
-        app,
+    app.run(
         host="0.0.0.0",
-        port=port
+        port=int(os.environ.get("PORT", 10000))
     )
